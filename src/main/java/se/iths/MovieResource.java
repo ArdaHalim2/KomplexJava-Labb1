3cfacf865f1b5d9eec6e78ccfebc374ed0fbb2a2
ArdaHalim2 <arda.halim@iths.se>
2025-02-27 14:01:39 +0100

    added MovieService

--- src/main/java/se/iths/MovieResource.java
+++ src/main/java/se/iths/MovieResource.java
@@ -1,11 +1,20 @@
 package se.iths;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello-world")
+@Path("/movies")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MovieResource {
+    @Inject
+    private MovieService movieService;
+
+
     @GET
     @Produces("text/plain")
     public String hello() {
